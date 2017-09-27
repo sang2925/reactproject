d7443f410ef5b117217caade0109ebdce49d1f23
Sebastian Ang <angdajiansebastian@gmail.com>
2017-09-27 09:02:11 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import "./App.css";
+
+const list = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react",
+    author: "Me",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://google.com",
+    author: "Me",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list, searchTerm: '',
+    };
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input type="text" onChange={this.onSearchChange} />
+        </form>
+        { this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author} </span>
+            <span> {item.num_comments} </span>
+            <span> {item.points} </span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dimiss Me
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
